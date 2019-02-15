4865518818d9c081bd43366ba60927d691e60bb8
Pedro Pombeiro <pombeirp@users.noreply.github.com>
2019-02-15 09:04:03 +0100

    Migrate Desktop build environment to leverage Nix
    - Add Conan to default.nix
    - Integrate Qt5 Nix package (with forked repo for new QtWebView module)
    - Fix rpaths in macOS and Linux
    - Add Nix expressions to install nsis, appimagekit and linuxdeployqt

---
+++ scripts/lib/setup/nix/desktop/default.nix
@@ -0,0 +1,24 @@
+{ stdenv, pkgs }:
+
+with pkgs;
+with stdenv; 
+
+let
+  windowsPlatform = callPackage ./windows { };
+  appimagekit = callPackage ./appimagetool { };
+  linuxdeployqt = callPackage ./linuxdeployqt { appimagekit = appimagekit; };
+
+in
+  {
+    buildInputs = [
+      cmake
+      extra-cmake-modules
+      go
+      qt5.full
+    ] ++ lib.optional isLinux [ appimagekit linuxdeployqt patchelf ]
+      ++ lib.optional isLinux windowsPlatform.buildInputs;
+    shellHook = ''
+      export QT_PATH="${qt5.full}"
+      export PATH="${stdenv.lib.makeBinPath [ qt5.full ]}:$PATH"
+    '';
+  }

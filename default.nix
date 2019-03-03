1c1f32967092b7f2a2a4e96ef93131b4f3544397
Pedro Pombeiro <pombeirp@users.noreply.github.com>
2019-03-03 01:34:06 +0100

    Add Android NDK Nix expression

--- default.nix
+++ default.nix
@@ -10,6 +10,7 @@ in with pkgs;
   let
     derivation = if stdenv.isLinux then stdenvNoCC.mkDerivation else stdenv.mkDerivation;
     statusDesktop = callPackage ./scripts/lib/setup/nix/desktop { };
+    statusMobile = callPackage ./scripts/lib/setup/nix/mobile { };
     nodeInputs = import ./scripts/lib/setup/nix/global-node-packages/output {
       # The remaining dependencies come from Nixpkgs
       inherit pkgs;
@@ -30,8 +31,8 @@ in with pkgs;
       jq
       leiningen
       maven
-      openjdk
       statusDesktop.buildInputs
+      statusMobile.buildInputs
       watchman
       unzip
       wget
@@ -40,17 +41,8 @@ in with pkgs;
       ++ lib.optional isLinux gcc7;
     shellHook = ''
         ${statusDesktop.shellHook}
+        ${statusMobile.shellHook}
 
-        local toolversion="$(git rev-parse --show-toplevel)/scripts/toolversion"
-
-        export JAVA_HOME="${openjdk}"
-        export ANDROID_HOME=~/.status/Android/Sdk
-        export ANDROID_SDK_ROOT="$ANDROID_HOME"
-        export ANDROID_NDK_ROOT="$ANDROID_SDK_ROOT/android-ndk-$($toolversion android-ndk)"
-        export ANDROID_NDK_HOME="$ANDROID_NDK_ROOT"
-        export ANDROID_NDK="$ANDROID_NDK_ROOT"
-        export PATH="$ANDROID_HOME/bin:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools:$ANDROID_HOME/build-tools:$PATH"
-
-        [ -d "$ANDROID_NDK_ROOT" ] || ./scripts/setup # we assume that if the Android NDK dir does not exist, make setup needs to be run
+        [ -d "$ANDROID_SDK_ROOT" ] || ./scripts/setup # we assume that if the Android SDK dir does not exist, make setup needs to be run
     '';
   }

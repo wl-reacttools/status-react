7bd13a7d91e46b25c8968bf0963ae7e884dbc9e2
Pedro Pombeiro <pombeirp@users.noreply.github.com>
2019-03-05 18:36:40 +0100

    Implement target-dependent installing of Nix dependencies

--- default.nix
+++ default.nix
@@ -1,16 +1,28 @@
-let
-  pkgs = import ((import <nixpkgs> { }).fetchFromGitHub {
+# target-os = [ 'windows' 'linux' 'macos' 'android' 'ios' ]
+{ pkgs ? import ((import <nixpkgs> { }).fetchFromGitHub {
     owner = "status-im";
     repo = "nixpkgs";
     rev = "5922f168965214ffdbc1404c081dca95b714840b";
     sha256 = "1jrvbf0903bs6a74x1nwxa6bx0q78a420mkw5104p2p5967lxkvg";
-  }) { config = { }; };
+  }) { config = { }; },
+  target-os ? "" }:
 
-in with pkgs;
+with pkgs;
   let
+    targetDesktop = {
+      "linux" = true;
+      "windows" = true;
+      "macos" = true;
+      "" = true;
+    }.${target-os} or false;
+    targetMobile = {
+      "android" = true;
+      "ios" = true;
+      "" = true;
+    }.${target-os} or false;
     derivation = if stdenv.isLinux then stdenvNoCC.mkDerivation else stdenv.mkDerivation;
-    statusDesktop = callPackage ./scripts/lib/setup/nix/desktop { };
-    statusMobile = callPackage ./scripts/lib/setup/nix/mobile { };
+    statusDesktop = callPackage ./scripts/lib/setup/nix/desktop { inherit target-os; };
+    statusMobile = callPackage ./scripts/lib/setup/nix/mobile { inherit target-os; };
     nodeInputs = import ./scripts/lib/setup/nix/global-node-packages/output {
       # The remaining dependencies come from Nixpkgs
       inherit pkgs;
@@ -33,18 +45,20 @@ in with pkgs;
       jq
       leiningen
       maven
-      statusDesktop.buildInputs
-      statusMobile.buildInputs
       watchman
       unzip
       wget
     ] ++ nodePkgs
       ++ lib.optional isDarwin cocoapods
-      ++ lib.optional isLinux gcc7;
-    shellHook = ''
-        ${statusDesktop.shellHook}
-        ${statusMobile.shellHook}
-
-        [ -d "$ANDROID_SDK_ROOT" ] || ./scripts/setup # we assume that if the Android SDK dir does not exist, make setup needs to be run
-    '';
+      ++ lib.optional isLinux gcc7
+      ++ lib.optional targetDesktop statusDesktop.buildInputs
+      ++ lib.optional targetMobile statusMobile.buildInputs;
+    shellHook =
+      (if targetDesktop then statusDesktop.shellHook else "") +
+      (if targetMobile then statusMobile.shellHook else "") +
+      ''
+        if [ -n "$ANDROID_SDK_ROOT" ] && [ ! -d "$ANDROID_SDK_ROOT" ]; then
+          ./scripts/setup # we assume that if the Android SDK dir does not exist, make setup needs to be run
+        fi
+      '';
   }
--- scripts/lib/setup/nix/desktop/default.nix
+++ scripts/lib/setup/nix/desktop/default.nix
@@ -1,9 +1,17 @@
-{ stdenv, pkgs }:
+{ stdenv, pkgs, target-os }:
 
 with pkgs;
 with stdenv; 
 
 let
+  targetLinux = {
+    "linux" = true;
+    "" = true;
+  }.${target-os} or false;
+  targetWindows = {
+    "windows" = true;
+    "" = true;
+  }.${target-os} or false;
   windowsPlatform = callPackage ./windows { };
   appimagekit = callPackage ./appimagetool { };
   linuxdeployqt = callPackage ./linuxdeployqt { appimagekit = appimagekit; };
@@ -14,11 +22,13 @@ in
       cmake
       extra-cmake-modules
       go
-      qt5.full
-    ] ++ lib.optional isLinux [ appimagekit linuxdeployqt patchelf ]
-      ++ lib.optional isLinux windowsPlatform.buildInputs;
-    shellHook = ''
+    ] ++ lib.optional targetLinux [ appimagekit linuxdeployqt patchelf ]
+      ++ lib.optional (! targetWindows) qt5.full
+      ++ lib.optional targetWindows windowsPlatform.buildInputs;
+    shellHook = (if target-os == "windows" then "unset QT_PATH" else ''
       export QT_PATH="${qt5.full}"
       export PATH="${qt5.full}/bin:$PATH"
-    '';
+    '') + (lib.optionalString isDarwin ''
+      export MACOSX_DEPLOYMENT_TARGET=10.9
+    '');
   }

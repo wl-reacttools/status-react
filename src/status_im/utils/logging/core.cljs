20bcbf2d3d567426334ad2b4339a525a09adbd1b
Roman Volosovskyi <roman.wolosovskyi@gmail.com>
2019-05-07 12:00:42 +0300

    App version and more detail in logs email

--- src/status_im/utils/logging/core.cljs
+++ src/status_im/utils/logging/core.cljs
@@ -7,7 +7,11 @@
             [status-im.utils.email :as mail]
             [taoensso.timbre :as log]
             [status-im.utils.config :as config]
-            [status-im.i18n :as i18n]))
+            [status-im.i18n :as i18n]
+            [status-im.utils.platform :as platform]
+            [status-im.utils.build :as build]
+            [status-im.transport.utils :as transport.utils]
+            [status-im.utils.datetime :as datetime]))
 
 (def report-email "error-reports@status.im")
 (def max-log-entries 1000)
@@ -95,8 +99,7 @@
         (transport.utils/extract-url-components (:enode node-info))]
     (clojure.string/join
      "\n"
-     (concat [(i18n/label :t/report-bug-email-template)]
-             [separator
+     (concat [separator
               (str "App version: " build-version)
               (str "OS: " platform/os)
               (str "Node version: " web3-node-version)
@@ -123,14 +126,14 @@
 
 (handlers/register-handler-fx
  ::send-email
- (fn [cofx [_ archive-path]]
+ (fn [{:keys [db] :as cofx} [_ archive-path]]
    (fx/merge
     cofx
     (dialog-closed)
     (mail/send-email
      {:subject    "Error report"
       :recipients [report-email]
-      :body       "logs attached"
+      :body       (email-body db)
       :attachment {:path archive-path
                    :type "zip"
                    :name "status_logs.zip"}}
